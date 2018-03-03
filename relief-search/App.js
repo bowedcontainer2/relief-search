c52963c93438680d3bbd29d1318d8157f0891b61
Matthew Pineda <mpineda3247@gmail.com>
2018-03-03 04:34:31 -0800

    Created unstylized welcome page, got page navigator working.

--- relief-search/App.js
+++ relief-search/App.js
@@ -1,23 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, Text, TextInput, View, Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+
+
+class HomeScreen extends React.Component {
+  constructor(props) {
+  super(props);
+  this.state = {text: ''};
+}
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{padding: 10}}>
+
+      <Text style={{fontSize: 50}}>Welcome to Relief Search.</Text>
+
+      <Text style={{padding: 5}}> Name of Missing Person.</Text>
+        <TextInput
+          style={{height: 30}}
+          placeholder="Full Name"
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Text style={{padding: 5}}>Best Phone Number.</Text>
+        <TextInput
+          style={{height: 30}}
+          placeholder="Phone Number"
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Button
+        onPress={() => this.props.navigation.navigate('PictureUpload')}
+        title="Next"
+        color="#79CDCD"
+        accessibilityLabel="Learn more about this purple button"
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+class PictureUpload extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    )
+  }
+}
+
+export default StackNavigator({
+  Home: {
+    screen: HomeScreen,
   },
+  PictureUpload: {
+    screen: PictureUpload,
+  }
 });
