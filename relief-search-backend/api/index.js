22ad553f6ee678373c862ea39402057f31053f94
Jack-Pham <phminhthong88@gmail.com>
2018-03-03 22:37:10 -0800

    add

--- relief-search-backend/api/index.js
+++ relief-search-backend/api/index.js
@@ -22,20 +22,17 @@ var upload = multer({
  }).single('image');
 
 
- var options = {
-     url: 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true',
-     method: 'POST',
-     headers: {
-         'Ocp-Apim-Subscription-Key': '6db02891df294caa88b462af42da79be',
-         'Content-Type': 'application/json'
-     },
-     body: JSON.stringify(obj)
- };
-
-
 function processImage(){
   obj["url"] = "http://35.185.245.7/Images/" + name;
-  console.log(uri);
+  var options = {
+      url: 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true',
+      method: 'POST',
+      headers: {
+          'Ocp-Apim-Subscription-Key': '6db02891df294caa88b462af42da79be',
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(obj)
+  };
   request(options, function(err, res, body) {
     if (err){
       console.log(err);
